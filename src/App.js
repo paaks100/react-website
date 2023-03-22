26e377981f0d5339a33d0c13ff1492aea480b1ce
paaks100 <pkkodua@gmail.com>
2023-03-22 10:18:37 +0000

    commit pre-comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/Footer';
 import {Navbar} from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/menu' element={<Menu/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
         <Footer/>
       </Router>
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { MenuList } from '../helpers/MenuList'
+import React from 'react';
+import MenuItem from '../components/MenuItem';
+import { MenuList } from '../helpers/MenuList';
+import '../styles/Menu.css';
 
 function Menu() {
   return (
     <div className='menu'>
         <h1 className='menuTitle'> Our Menu </h1>
         <div className='menuList'>{MenuList.map((menuItem, key) => {
-            return <div> {menuItem.name} </div>
+            return (
+                <MenuItem
+                    key={key}
+                    image={menuItem.image}
+                    name={menuItem.name}
+                    price={menuItem.price}
+                />
+            )
         })}</div>
     </div>
   )
